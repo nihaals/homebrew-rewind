2f0dbd5235faeda8ee797bdaae4da61290939e40
Nihaal Sangha <nihaal.git@gmail.com>
2023-03-31 14:44:58 +0100

    Change to dmg

--- Casks/rewind.rb
+++ Casks/rewind.rb
@@ -2,7 +2,7 @@ cask "rewind" do
   version :latest
   sha256 :no_check
 
-  url "https://download.rewind.ai/Rewind.pkg"
+  url "https://download.rewind.ai/Rewind.dmg"
   name "Rewind"
   desc "Record and search your screen and audio"
   homepage "https://www.rewind.ai/"
@@ -10,28 +10,9 @@ cask "rewind" do
   depends_on arch:  :arm64,
              macos: ">= :monterey"
 
-  pkg "Rewind.pkg"
+  app "Rewind.app"
 
-  postflight do
-    # Description: Ensure console variant of postinstall is non-interactive.
-    # This is because `open "${app_dir}"` is called from the postinstall
-    # script of the package and we don't want any user intervention there.
-    retries ||= 3
-    ohai "The Rewind package postinstall script launches the Rewind app" unless retries < 3
-    ohai "Attempting to close Rewind.app to avoid unwanted user intervention" unless retries < 3
-    return unless system_command "/usr/bin/pkill", args: ["-f", "/Applications/Rewind.app"]
-
-  rescue RuntimeError
-    sleep 1
-    retry unless (retries -= 1).zero?
-    opoo "Unable to forcibly close Rewind.app"
-  end
-
-  uninstall pkgutil:    "com.memoryvault.MemoryVault",
-            quit:       "com.memoryvault.MemoryVault",
-            launchctl:  "com.rewind.Rewind",
-            delete:     "/Applications/Rewind.app",
-            login_item: "Rewind"
+  uninstall quit: "com.memoryvault.MemoryVault"
 
   zap trash: [
     "~/Documents/rewind_logs_*.zip",
